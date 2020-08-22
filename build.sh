654206428c0ccfbead3bacd3e40c400bf9037b3f
杨金鹏_18731 <18731@etransfar.com>
2020-08-22 21:24:16 +0800

    init

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-DOCKER_TAG=stkreport/stkreport:latest
+DOCKER_TAG=317491841/stkreport:latest
 
 echo " docker build -f Dockerfile -t ${DOCKER_TAG} ."
 docker build -f Dockerfile -t ${DOCKER_TAG} .
 echo "#################################################################"
 echo " docker push ${DOCKER_TAG} "
+docker push ${DOCKER_TAG}
